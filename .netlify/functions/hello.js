33863c7a283f909cf200cd3da8e71b9b7de25219
Brandon Kase <brandon.kase@gmail.com>
2020-01-20 01:06:03 -0800

    Try to load hello world

--- .netlify/functions/hello.js
+++ .netlify/functions/hello.js
@@ -1,3 +1,8 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {statusCode : 200, body : "Hello, World"});
+const fs = require('fs');
+
+exports.handler = async function(event, context) {
+  const buffer = await fs.readFile("./hello.wasm");
+  const module = await WebAssembly.compile(buffer);
+  const instance = await WebAssembly.instantiate(module);
+  callback(null, {statusCode : 200, body : "Hello, World " + instance.exports.helloWorld()});
 };
