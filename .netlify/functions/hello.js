1d27a7c46f13a769f8c154ff6488ed770d73886c
Brandon Kase <brandon.kase@gmail.com>
2020-01-20 01:17:03 -0800

    Uses __dirname for relative path

--- .netlify/functions/hello.js
+++ .netlify/functions/hello.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 const util = require('util');
 
 const readFile = util.promisify(fs.readFile);
 
 exports.handler = async function(event, context) {
-  const buffer = await readFile("./hello.wasm");
+  const buffer = await readFile(path.join(__dirname, "hello.wasm"));
   const module = await WebAssembly.compile(buffer);
   const instance = await WebAssembly.instantiate(module);
   return {statusCode : 200, body : "Hello, World " + instance.exports.helloWorld()};
 };
+
+exports.handler().then(console.log);
 
