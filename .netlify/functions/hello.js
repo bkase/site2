e351826d86e4ea09428d0a5f2d611187d2ec5193
Brandon Kase <brandon.kase@gmail.com>
2020-01-20 01:12:42 -0800

    Promisify the readFile

--- .netlify/functions/hello.js
+++ .netlify/functions/hello.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 exports.handler = async function(event, context) {
-  const buffer = await fs.readFile("./hello.wasm");
+  const buffer = await readFile("./hello.wasm");
   const module = await WebAssembly.compile(buffer);
   const instance = await WebAssembly.instantiate(module);
-  callback(null, {statusCode : 200, body : "Hello, World " + instance.exports.helloWorld()});
+  return {statusCode : 200, body : "Hello, World " + instance.exports.helloWorld()};
 };
+
